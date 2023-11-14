7e215b543cb39d11a07ee9520cd410a64cf23979
oscrowd <oscrowd@gmail.com>
2023-11-15 00:17:06 +0300

    D-01.000.02 ФормыРегистрации

---
+++ AwesomeNetworkMy/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeNetworkMy.Models;
+using AwesomeNetworkMy.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AwesomeNetworkMy.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
