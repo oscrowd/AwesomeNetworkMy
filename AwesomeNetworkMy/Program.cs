07dadc312a74aa4aeb32a73d92929ff792a97552
oscrowd <oscrowd@gmail.com>
2023-11-16 00:01:35 +0300

    D-01.000.06 Рабочий проект. Главный экран

--- AwesomeNetworkMy/Program.cs
+++ AwesomeNetworkMy/Program.cs
@@ -1,49 +1,71 @@
 using AwesomeNetworkMy.Models;
-using AwesomeNetworkMy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-//ад
-string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection))
-       .AddIdentity<User, IdentityRole>(opts =>
-       {
-           opts.Password.RequiredLength = 5;
-           opts.Password.RequireNonAlphanumeric = false;
-           opts.Password.RequireLowercase = false;
-           opts.Password.RequireUppercase = false;
-           opts.Password.RequireDigit = false;
-       })
-           .AddEntityFrameworkStores<ApplicationDbContext>();
+using AutoMapper;
 
 
-
-// Add services to the container.
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace AwesomeNetworkMy
 {
-    app.UseExceptionHandler("/Home/Error");
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            //ад
+            string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+            var mapperConfig = new MapperConfiguration((v) =>
+            {
+                v.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            builder.Services.AddSingleton(mapper);
+
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection))
+                   .AddIdentity<User, IdentityRole>(opts =>
+                   {
+                       opts.Password.RequiredLength = 5;
+                       opts.Password.RequireNonAlphanumeric = false;
+                       opts.Password.RequireLowercase = false;
+                       opts.Password.RequireUppercase = false;
+                       opts.Password.RequireDigit = false;
+                   })
+                       .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
